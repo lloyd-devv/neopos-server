0c34d5f964626a4dafd8ce369873f7cba1df84ab
lloyd-devv <muhammadfatiha83@gmail.com>
2026-02-17 20:04:17 +0800

    Refactor server.js for improved readability

--- server.js
+++ server.js
@@ -128,3 +128,4 @@ app.get('/', (req, res) => res.sendFile(path.join(__dirname, 'public', 'index.ht
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
 
+
